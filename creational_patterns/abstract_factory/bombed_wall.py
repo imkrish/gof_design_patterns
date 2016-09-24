b38741d9720c28ad7294498f6e645241fc996b8b
Keerati Limkulphong <imKrish@Keeratis-MacBook-Pro.local>
2016-09-24 18:47:57 +1200

    Add abstract factory diagram

--- creational_patterns/abstract_factory/bombed_wall.py
+++ creational_patterns/abstract_factory/bombed_wall.py
@@ -4,4 +4,5 @@ from creational_patterns.abstract_factory.wall import Wall
 class BombedWall(Wall):
 
     def __init__(self):
+        super().__init__()
         print('Bombed wall\'s been created ')
--- creational_patterns/abstract_factory/room.py
+++ creational_patterns/abstract_factory/room.py
@@ -11,18 +11,18 @@ class Room(metaclass=ABCMeta):
     WEST = 4
 
     def __init__(self):
-        self.sides = {}
+        self.__sides = {}
         print('Room\'s been created.')
 
     def set_side(self, side, wall_or_door):
         assert isinstance(wall_or_door, Wall) or isinstance(wall_or_door, Door)
         if side == Room.NORTH:
-            self.sides[Room.NORTH] = wall_or_door
+            self.__sides[Room.NORTH] = wall_or_door
         elif side == Room.EAST:
-            self.sides[Room.EAST] = wall_or_door
+            self.__sides[Room.EAST] = wall_or_door
         elif side == Room.SOUTH:
-            self.sides[Room.SOUTH] = wall_or_door
+            self.__sides[Room.SOUTH] = wall_or_door
         elif side == Room.WEST:
-            self.sides[Room.WEST] = wall_or_door
+            self.__sides[Room.WEST] = wall_or_door
         else:
             raise ValueError('Value for side is invalid.')
