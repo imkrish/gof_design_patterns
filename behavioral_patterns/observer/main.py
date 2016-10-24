d384dc93e057c79583092b1a94fcb7c0cc4c2182
Keerati Limkulphong <imkrish@Keeratis-MacBook-Pro.local>
2016-10-25 08:19:29 +1300

    update main.py for presentation

--- behavioral_patterns/observer/main.py
+++ behavioral_patterns/observer/main.py
@@ -8,16 +8,21 @@ if __name__ == '__main__':
 
     subject = ClockTimer()  # Creates a subject object
 
-    digital_clock1 = DigitalClock(subject)  # Creates an observer object and attaches its self to the subject
-    analog_clock1 = AnalogClock(subject)  # Creates an observer object and attaches its self to the subject
-
-    analog_clock1.unsubscribe()
+    digital_clock1 = DigitalClock(subject)  # Creates an observer object and attaches itself to the subject
+    analog_clock1 = AnalogClock(subject)  # Creates an observer object and attaches itself to the subject
 
     thread = threading.Timer(100, subject.cancel_interval)  # After 100 seconds will stop the subject from notifying 
     thread.start()
 
-    # digital_clock2 = DigitalClock(subject)
-    # analog_clock2 = AnalogClock(subject)
+    thread2 = threading.Timer(7, analog_clock1.unsubscribe)
+    thread2.start()
+
+    def add_two_more_observers():
+        digital_clock2 = DigitalClock(subject)
+        analog_clock2 = AnalogClock(subject) 
+    
+    thread3 = threading.Timer(12, add_two_more_observers)
+    thread3.start()
 
 
 
