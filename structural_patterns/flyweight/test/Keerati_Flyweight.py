e14d02053567f38fc92309bea92f23b54959c1c5
Keerati Limkulphong <imkrish@Keeratis-MacBook-Pro.local>
2016-10-26 10:55:38 +1300

    A small test for flyweight pattern

---
+++ structural_patterns/flyweight/test/Keerati_Flyweight.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+
+
+class MonsterFactory(object):
+
+    """ConcreteFlyweightFactory"""
+
+    def __init__(self):
+        self.__monsters = {}
+
+    def get_monster(self, monster_type):
+        if monster_type not in self.__monsters:
+            new_monster = eval(monster_type + '()')
+            # Check whether this object is an instance of Monster.
+            assert isinstance(new_monster, Monster)
+            # Then assign to a monster pool.
+            self.__monsters[monster_type] = new_monster
+
+        return self.__monsters[monster_type]
+
+
+class Monster(ABC):
+
+    """Flyweight"""
+
+    def __init__(self, name):
+        self.__name = name
+
+    @abstractmethod
+    def draw(self, x, y, z):
+        """ Default Implementation """
+        print('A {} is drawn at {}, {}, {}.'.format(self.__name, x, y, z))
+
+
+class Orc(Monster):
+
+    """Concrete Flyweight"""
+
+    def __init__(self):
+        super().__init__('Orc')
+
+    def draw(self, x, y, z):
+        # Use default implementation from parent class
+        super().draw(x, y, z)
+
+
+class Goblin(Monster):
+
+    """Concrete Flyweight"""
+
+    def __init__(self):
+        super().__init__('Golbin')
+
+    def draw(self, x, y, z):
+        # Use default implementation from parent class
+        super().draw(x, y, z)
+
+
+def client():
+
+    """Client uses FlyweightFactory object to get the ConcreteFlyweight objects from the pool"""
+
+    monster_factory = MonsterFactory()
+    monster1 = monster_factory.get_monster('Orc')
+    monster1.draw(1, 3, 5)
+
+    monster2 = monster_factory.get_monster('Orc')
+    monster2.draw(2, 5, 1)
+
+    monster3 = monster_factory.get_monster('Orc')
+    monster3.draw(3, 4, 6)
+
+    monster4 = monster_factory.get_monster('Goblin')
+    monster4.draw(1, 1, 7)
+
+    monster5 = monster_factory.get_monster('Goblin')
+    monster5.draw(1, 3, 4)
+
+    monster6 = monster_factory.get_monster('Goblin')
+    monster6.draw(3, 4, 6)
+
+
+if __name__ == '__main__':
+    client()
+
